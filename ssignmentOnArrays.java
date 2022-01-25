ae8ef32d605992f9a4927b24ecc8af86cdf6baf6
Nikhil Kumar <50484204+nikhil2222@users.noreply.github.com>
2022-01-25 16:56:04 +0530

    Assignments on array
    Done till 21st

---
+++ ssignmentOnArrays.java
@@ -0,0 +1,41 @@
+package assignment.com;
+//Assignment 1: Rotate the Pixels to the Right by 90 degree :)
+		//			  2: Rotate the Pixels to the Left by 180 degrees
+		//			  3: Grayscale the image -> eg: pixel1 = {255, 0, 0};
+		//					we will add 255+0+0/3 -> 85
+		//					now pixel1 will be -> {85, 85, 85}, so d it for all the pixels
+
+
+public class ssignmentOnArrays {
+
+	public static void main(String[] args) {
+		int[] pixel1 = {255, 0, 0}; // red :)
+		int[] pixel2 = {123, 76, 44};
+		int[] pixel3 = {32, 120, 0};
+		int[] pixel4 = {123, 12, 120};
+		int[] pixel5 = {189, 18, 0};
+		int[] pixel6 = {187, 56, 130};
+		int[] pixel7 = {140, 76, 120};
+		int[] pixel8 = {90, 123, 90};
+		int[] pixel9 = {200, 145, 105};
+		
+		// Image is an Array -> 3D
+		int[][][] image = {
+						{pixel1, pixel2, pixel3},
+						{pixel4, pixel5, pixel6},
+						{pixel7, pixel8, pixel9}
+					};
+		int[][][] image = {
+				{pixel7, pixel4, pixel1},
+				{pixel8, pixel5, pixel2},
+				{pixel9, pixel6, pixel3}
+			};
+		int[][][] image = {
+				{pixel9, pixel8, pixel7},
+				{pixel6, pixel5, pixel4},
+				{pixel3, pixel2, pixel1}
+			};
+
+	}
+
+}
